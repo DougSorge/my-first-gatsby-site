301337f1e4f106a170dcbcfc5a23d9665ccbfcf2
Doug S <sorged91@gmail.com>
2021-10-27 22:19:16 -0400

    css modules work slightly differently in Gatsby then creat-react-app. They are closer to react-strap imports.

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+export default function About() {
+  return (
+    <Layout pageTitle="About">
+      <p>
+        Hello everyone! I'm excited to be jumping into Gatsby after learning how
+        to use React without it.
+      </p>
+      <p>
+        This will allow me to pursue my dream of starting my own Develop Agency
+        one day. Fast Builds, Quality Content, Great Designs, Peak Performance.
+      </p>
+    </Layout>
+  );
+}
